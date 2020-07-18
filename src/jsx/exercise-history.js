61a8cb4ab50b1adc8e43c43deba817277be2f924
EDEV <edev@EDEVs-MacBook-Air.local>
2020-07-18 15:27:37 -0400

    Started adding some custom react components

---
+++ src/jsx/exercise-history.js
@@ -0,0 +1,89 @@
+function initializeHistoryChart(){
+    $.get('/api/exercise_history', {exercise_id: 3}, (data) => {
+        context = document.getElementById('exercise-history-chart').getContext('2d');
+        chart = new Chart(context, {
+            type: "line",
+            data: {
+                labels: data.dates,
+                datasets: [{
+                    data: data.weights,
+                    backgroundColor: '#ff000099'
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                }
+            }
+        });
+    });
+
+}
+
+function exerciseInfo(){
+    const [info, setInfo] = React.useState(Array());
+    
+    React.useEffect(() => {
+        $.get('/api/exercises', (data) => {
+            setInfo(data.exercises);
+        });
+    });
+
+    return info;
+}
+
+function ExerciseSelector(props){
+    const exercises = exerciseInfo().map(
+        ex => 
+                <div
+                    className="dropdown-item"
+                >
+                    {ex}
+                </div>
+            );
+
+    return <div className="dropdown">
+        <div
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            key="dropdownMenuButton"
+        >
+            Dropdown Toggle
+        </div>
+        <div className="dropdown-menu" aria-labelledby="dropdown">
+            { exercises }
+        </div>
+    </div>
+}
+
+function Card(props){
+    return <div className="card">
+        <div className="card-header">
+            <h5 className="card-title">{props.title}</h5>
+            {
+                props.category &&
+                <p className="card-category">{props.category}</p>
+            }
+        </div>
+        <div className="card-body">
+            {props.children}
+        </div>
+    </div>
+}
+
+function GraphCard(props){
+    return <Card title="test" >
+        <canvas id={props.id} style=`width: ${};height: ${}`></canvas>
+    </Card>
+}
+
+window.onload = () => {
+    initializeHistoryChart();
+    ReactDOM.render(
+        <GraphCard />,
+        document.getElementById("placeholder")
+    );
+};
--- statdashboard/static/assets/js/exercise-history.js
+++ statdashboard/static/assets/js/exercise-history.js
@@ -1,5 +1,7 @@
-function initializeHistoryChart(){
-    $.get('/api/exercise_history', {exercise_id: 3}, (data) => {
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+function initializeHistoryChart() {
+    $.get('/api/exercise_history', { exercise_id: 3 }, function (data) {
         context = document.getElementById('exercise-history-chart').getContext('2d');
         chart = new Chart(context, {
             type: "line",
@@ -17,62 +19,92 @@ function initializeHistoryChart(){
             }
         });
     });
-
 }
 
-function getExerciseInfo(){
-    var prom = $.get('/api/exercises', (data) => {
-        console.log(data);
-        var k = data.exercises;
-    });
-    return prom;
-}
+function exerciseInfo() {
+    var _React$useState = React.useState(Array()),
+        _React$useState2 = _slicedToArray(_React$useState, 2),
+        info = _React$useState2[0],
+        setInfo = _React$useState2[1];
 
-class ExerciseSelector extends React.Component {
-    constructor(props){
-        super(props);
-    }
-
-    handleClick(i){
-        return;
-    }
-
-    render(){
-        let childArray = Array();
-        childArray.push(React.createElement('div', {
-                    className: 'btn btn-secondary dropdown-toggle',
-                    type: 'button',
-                    'data-toggle': 'dropdown',
-                    'aria-haspopup': true,
-                    'aria-expanded': false,
-                    key: 'dropdownMenuButton'
-                },
-                'Dropdown toggle'
-            )
-        );
-
-        getExerciseInfo().done((data) => {
-            let exercises = data;
+    React.useEffect(function () {
+        $.get('/api/exercises', function (data) {
+            setInfo(data.exercises);
         });
-        for (let i = 0; i < exercises.length; i++){
-            let el = React.createElement('div', {
-                    className: 'dropdown-item',
-                    'aria-labelledBy': 'dropdown'
-                },
-                exercises[i]
-            );
-            childArray.push(el);
-        }
-        return React.createElement('div', {
-            className: 'dropdown',
-        }, childArray);
-    }
+    });
+
+    return info;
 }
 
-window.onload = () => {
-    initializeHistoryChart();
-    ReactDOM.render(
-        React.createElement(ExerciseSelector, null),
-        document.getElementById('placeholder')
+function ExerciseSelector(props) {
+    var exercises = exerciseInfo().map(function (ex) {
+        return React.createElement(
+            'div',
+            {
+                className: 'dropdown-item'
+            },
+            ex
+        );
+    });
+
+    return React.createElement(
+        'div',
+        { className: 'dropdown' },
+        React.createElement(
+            'div',
+            {
+                className: 'btn btn-secondary dropdown-toggle',
+                type: 'button',
+                'data-toggle': 'dropdown',
+                'aria-haspopup': 'true',
+                'aria-expanded': 'false',
+                key: 'dropdownMenuButton'
+            },
+            'Dropdown Toggle'
+        ),
+        React.createElement(
+            'div',
+            { className: 'dropdown-menu', 'aria-labelledby': 'dropdown' },
+            exercises
+        )
     );
+}
+
+function Card(props) {
+    return React.createElement(
+        'div',
+        { className: 'card' },
+        React.createElement(
+            'div',
+            { className: 'card-header' },
+            React.createElement(
+                'h5',
+                { className: 'card-title' },
+                props.title
+            ),
+            props.category && React.createElement(
+                'p',
+                { className: 'card-category' },
+                props.category
+            )
+        ),
+        React.createElement(
+            'div',
+            { className: 'card-body' },
+            props.children
+        )
+    );
+}
+
+function GraphCard(props) {
+    return React.createElement(
+        Card,
+        { title: 'test' },
+        'test'
+    );
+}
+
+window.onload = function () {
+    initializeHistoryChart();
+    ReactDOM.render(React.createElement(GraphCard, null), document.getElementById("placeholder"));
 };
